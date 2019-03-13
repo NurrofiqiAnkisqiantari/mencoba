fc95031b60401bf4ce2aef3474404678f98f4122
NurrofiqiAnkisqiantari <nurrofiqi.2310@students.amikom.ac.id>
2019-03-13 13:05:08 +0700

    mencoba

---
+++ biodata.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	char nama [15] = "nurrofiqi";
+	int umur = 18;
+	float berat = 63;
+	cout<< "nama : "<<nama<<endl;
+	cout<< "umur : "<<umur<< "tahun"<<endl;
+	cout<< "berat : "<<berat<< "kg"<<endl;
+return 0;
+} 
